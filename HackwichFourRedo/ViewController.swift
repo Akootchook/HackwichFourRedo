aaef7a11e8ac8a2d1bf3ac515a947db501ed14ea
Akootchook <77140613+Akootchook@users.noreply.github.com>
2021-02-13 17:39:21 -1000

    Successfully added IBOutlet Part 7 of Hackwich Four 13FEB2021

--- HackwichFourRedo/ViewController.swift
+++ HackwichFourRedo/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstTabLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        firstTabLabel.text = "About"
     }
 
 
